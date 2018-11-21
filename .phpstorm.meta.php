74f1415229d7ed6d69ef91a4bc2058fe8258e34f
rahee <ra256@njit.edu>
2018-11-20 23:24:58 -0500

    Laravel 5.6 Plugin was added

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-11-21 01:16:04.
+    * Generated on 2018-11-21 04:20:17.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.39 on 2018-11-21 01:16:02.
+ * Generated for Laravel 5.6.39 on 2018-11-21 04:20:16.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
