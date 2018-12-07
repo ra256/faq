6dc81cf881700c5fcd60e785568489a0ef21604c
rahee <ra256@njit.edu>
2018-12-07 01:04:22 -0500

    Task: Added logic to seeder to create admin account

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,5 +12,15 @@ class UsersTableSeeder extends Seeder
         factory(App\User::class, 50)->create()->each(function ($user) {
             $user->profile()->save(factory(App\Profile::class)->make());
         });
+
+        //Add Admin
+        DB::table('users')->insert([
+            'email' => 'admin@raheelminiproject3.herokuapp.com',
+            'password' => bcrypt('admin'),
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+            'locked' => 0,
+            'permission' => 1
+        ]);
     }
 }
