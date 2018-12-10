122a0afe28fd9e362739705c5853a5ea2a5cbe81
rahee <ra256@njit.edu>
2018-12-10 00:45:37 -0500

    Task: Finished adding the rest of the tests including promote, demote, and more. Finished read me file with instructions on testing.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # faq
 
-A Super Admin has all privileges, including giving admin access to all users
-An Admin User can lock and delete accounts but can not give admin access to other users.
-A standard users cannot view the admin URL as they will get an error message.
-All users can create, edit, delete questions and create a profile.
+1) A Super Admin has all privileges, including giving admin access to all users.
+2) An Admin User can lock and unlock accounts but can not give admin access to other users.
+3) A standard users cannot view the admin URL as they will get an error message.
+4) Admins can delete a user.
 
 To use the Super Admin, log in with the following credentials:
 
@@ -11,12 +11,9 @@ admin@raheelminiproject3.herokuapp.com
 Password is admin
 
 Hit the drop down to access the Admin Panel, and give it a go by giving new registered users admin access or demote their access.
-
 Try logging in as an admin after granting a registered user access, and notice the difference between the Super Admin and Admin options
-
 The Admin cannot create other Admin users, but they can still lock and delete users.
-
-There are flags in the database for the types of users, as below:
+There are flags in the database for the types of users, as below and you can see that in the testing as well.
 
 0 normal user
 2 admin
@@ -24,6 +21,42 @@ There are flags in the database for the types of users, as below:
 ___________
 0 unlock
 1 lock
+
+Testing instructions:
+
+To run tests: run two terminal windows - 1 to run the local site, php artisan serve and 2 to run php artisan dusk command to run the tests
+
+I have 1 test to check the super admin access to admin area
+testSuperAdminLogin
+
+I have 1 test to check the admin access to admin area
+testAdminLogin
+
+I have 1 test to check the delete user functionality.
+testUserDeleteFunctionality
+
+I have 1 test to check the lock user functionality.
+testUserLockFunctionality
+
+I have 1 test to check the unlock user functionality.
+testUserUnlockFunctionality
+
+I have 1 test to check the promote user functionality.
+testUserPromoteFunctionality
+
+I have 1 test to check the demote user functionality.
+testUserDemoteFunctionality
+
+I have 5 tests to make sure normal user doesn't have access to admin routes
+testNormalUserDeletePageAccess
+testNormalUserUnlockPageAccess
+testNormalUserLockPageAccess
+testNormalUserPromotePageAccess
+testNormalUserDemotePageAccess
+
+I have 2 tests to make sure admin doesn't have access to super-admin routes
+testAdminPromotePageAccess
+testAdminDemotePageAccess
 
 To run the FAQ project:
 
--- tests/Browser/AdminTest.php
+++ tests/Browser/AdminTest.php
@@ -54,12 +54,161 @@ class AdminTest extends DuskTestCase
         $user->delete();
     }
 
+
+    /**
+     * Test the user delete functionality in the admin panel
+     *
+     * @return void
+     */
+    public function testUserDeleteFunctionality()
+    {
+        $userToBeDeleted = factory(\App\User::class)->create([
+            'email' => 'normalusertest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 0 //normal user permission
+        ]);
+        $user = factory(\App\User::class)->create([
+            'email' => 'admintest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 2 //admin permission
+        ]);
+        $this->browse(function (Browser $browser) use($userToBeDeleted) {
+            $browser->visit('/login')
+                ->type('email', 'admintest@localhost.com')
+                ->type('password', 'testpass')
+                ->press('Login')
+                ->visit('/admin/delete/'.$userToBeDeleted->id)
+                ->assertSee('User has been deleted!');
+        });
+        $user->delete();
+    }
+
+    /**
+     * Test the user lock functionality in the admin panel
+     *
+     * @return void
+     */
+    public function testUserLockFunctionality()
+    {
+        $userToBeLocked = factory(\App\User::class)->create([
+            'email' => 'normalusertest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 0 //normal user permission
+        ]);
+        $user = factory(\App\User::class)->create([
+            'email' => 'admintest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 2 //admin permission
+        ]);
+        $this->browse(function (Browser $browser) use($userToBeLocked) {
+            $browser->visit('/login')
+                ->type('email', 'admintest@localhost.com')
+                ->type('password', 'testpass')
+                ->press('Login')
+                ->visit('/admin/lock/'.$userToBeLocked->id)
+                ->assertSee('User has been locked!');
+        });
+        $userToBeLocked->delete();
+        $user->delete();
+    }
+
+
+    /**
+     * Test the user unlock functionality in the admin panel
+     *
+     * @return void
+     */
+    public function testUserUnlockFunctionality()
+    {
+        $userToBeUnlocked = factory(\App\User::class)->create([
+            'email' => 'normalusertest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 0 //normal user permission
+        ]);
+        $user = factory(\App\User::class)->create([
+            'email' => 'admintest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 2 //admin permission
+        ]);
+        $this->browse(function (Browser $browser) use($userToBeUnlocked) {
+            $browser->visit('/login')
+                ->type('email', 'admintest@localhost.com')
+                ->type('password', 'testpass')
+                ->press('Login')
+                ->visit('/admin/unlock/'.$userToBeUnlocked->id)
+                ->assertSee('User has been unlocked!');
+        });
+        $userToBeUnlocked->delete();
+        $user->delete();
+    }
+
+
+    /**
+     * Test the user promote functionality in the super-admin panel
+     *
+     * @return void
+     */
+    public function testUserPromoteFunctionality()
+    {
+        $userToBePromoted = factory(\App\User::class)->create([
+            'email' => 'normalusertest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 0 //normal user permission
+        ]);
+        $user = factory(\App\User::class)->create([
+            'email' => 'admintest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 1 //super-admin permission
+        ]);
+        $this->browse(function (Browser $browser) use($userToBePromoted) {
+            $browser->visit('/login')
+                ->type('email', 'admintest@localhost.com')
+                ->type('password', 'testpass')
+                ->press('Login')
+                ->visit('/admin/promote/'.$userToBePromoted->id)
+                ->assertSee('User has been promoted!');
+        });
+        $userToBePromoted->delete();
+        $user->delete();
+    }
+
+
+    /**
+     * Test the user demote functionality in the super-admin panel
+     *
+     * @return void
+     */
+    public function testUserDemoteFunctionality()
+    {
+        $userToBeDemoted = factory(\App\User::class)->create([
+            'email' => 'normalusertest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 0 //normal user permission
+        ]);
+        $user = factory(\App\User::class)->create([
+            'email' => 'admintest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 1 //super-admin permission
+        ]);
+        $this->browse(function (Browser $browser) use($userToBeDemoted) {
+            $browser->visit('/login')
+                ->type('email', 'admintest@localhost.com')
+                ->type('password', 'testpass')
+                ->press('Login')
+                ->visit('/admin/demote/'.$userToBeDemoted->id)
+                ->assertSee('User has been demoted!');
+        });
+        $userToBeDemoted->delete();
+        $user->delete();
+    }
+
+
     /**
      * Test direct access of normal user to admin path
      * Normal users shouldn't be able to access the admin page
      * @return void
      */
-    public function NormalUserAdminAccess()
+    public function testNormalUserAdminPageAccess()
     {
         $user = factory(\App\User::class)->create([
             'email' => 'normalusertest@localhost.com',
@@ -73,6 +222,173 @@ class AdminTest extends DuskTestCase
                 ->press('Login')
                 ->visit('/admin')
                 ->assertSee('Sorry, you\'re not the Administrator');
+        });
+        $user->delete();
+    }
+
+    /**
+     * Test direct access of normal user to admin/delete path
+     * Normal users shouldn't be able to access the admin/delete page
+     * @return void
+     */
+    public function testNormalUserDeletePageAccess()
+    {
+        $user = factory(\App\User::class)->create([
+            'email' => 'normalusertest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 0 //user permission
+        ]);
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->type('email', 'normalusertest@localhost.com')
+                ->type('password', 'testpass')
+                ->press('Login')
+                ->visit('/admin/delete/1')
+                ->assertSee('Sorry, you\'re not the Administrator');
+        });
+        $user->delete();
+    }
+
+
+    /**
+     * Test direct access of normal user to admin/unlock path
+     * Normal users shouldn't be able to access the admin/unlock page
+     * @return void
+     */
+    public function testNormalUserUnlockPageAccess()
+    {
+        $user = factory(\App\User::class)->create([
+            'email' => 'normalusertest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 0 //user permission
+        ]);
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->type('email', 'normalusertest@localhost.com')
+                ->type('password', 'testpass')
+                ->press('Login')
+                ->visit('/admin/unlock/1')
+                ->assertSee('Sorry, you\'re not the Administrator');
+        });
+        $user->delete();
+    }
+
+
+    /**
+     * Test direct access of normal user to admin/lock path
+     * Normal users shouldn't be able to access the admin/lock page
+     * @return void
+     */
+    public function testNormalUserLockPageAccess()
+    {
+        $user = factory(\App\User::class)->create([
+            'email' => 'normalusertest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 0 //user permission
+        ]);
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->type('email', 'normalusertest@localhost.com')
+                ->type('password', 'testpass')
+                ->press('Login')
+                ->visit('/admin/lock/1')
+                ->assertSee('Sorry, you\'re not the Administrator');
+        });
+        $user->delete();
+    }
+
+
+    /**
+     * Test direct access of normal user to admin/promote path
+     * Normal users shouldn't be able to access the admin/promote page
+     * @return void
+     */
+    public function testNormalUserPromotePageAccess()
+    {
+        $user = factory(\App\User::class)->create([
+            'email' => 'normalusertest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 0 //user permission
+        ]);
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->type('email', 'normalusertest@localhost.com')
+                ->type('password', 'testpass')
+                ->press('Login')
+                ->visit('/admin/promote/1')
+                ->assertSee('Sorry, you\'re not the Super Administrator');
+        });
+        $user->delete();
+    }
+
+
+    /**
+     * Test direct access of normal user to admin/demote path
+     * Normal users shouldn't be able to access the admin/demote page
+     * @return void
+     */
+    public function testNormalUserDemotePageAccess()
+    {
+        $user = factory(\App\User::class)->create([
+            'email' => 'normalusertest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 0 //user permission
+        ]);
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->type('email', 'normalusertest@localhost.com')
+                ->type('password', 'testpass')
+                ->press('Login')
+                ->visit('/admin/demote/1')
+                ->assertSee('Sorry, you\'re not the Super Administrator');
+        });
+        $user->delete();
+    }
+
+
+    /**
+     * Test direct access of admin user to admin/promote path
+     * Admin shouldn't be able to access the admin/promote page (Only super admin can access this route)
+     * @return void
+     */
+    public function testAdminPromotePageAccess()
+    {
+        $user = factory(\App\User::class)->create([
+            'email' => 'admintest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 2 //admin permission
+        ]);
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->type('email', 'admintest@localhost.com')
+                ->type('password', 'testpass')
+                ->press('Login')
+                ->visit('/admin/promote/1')
+                ->assertSee('Sorry, you\'re not the Super Administrator');
+        });
+        $user->delete();
+    }
+
+
+    /**
+     * Test direct access of admin to admin/demote path
+     * Admin shouldn't be able to access the admin/demote page (Only super admin can access this route)
+     * @return void
+     */
+    public function testAdminDemotePageAccess()
+    {
+        $user = factory(\App\User::class)->create([
+            'email' => 'admintest@localhost.com',
+            'password' => bcrypt('testpass'),
+            'permission' => 0 //user permission
+        ]);
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->type('email', 'admintest@localhost.com')
+                ->type('password', 'testpass')
+                ->press('Login')
+                ->visit('/admin/demote/1')
+                ->assertSee('Sorry, you\'re not the Super Administrator');
         });
         $user->delete();
     }
