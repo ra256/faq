2d8efa0a7ab49a9755f7613f6fbec56de4b43dc4
rahee <ra256@njit.edu>
2018-12-11 17:41:44 -0500

    Task: Fixed Super Admin permission settings.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,12 +20,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        factory(\App\User::class)->create([
-            'email' => 'admin@raheelminiproject3.herokuapp.com',
-            'password' => bcrypt('admin'),
-            'permission' => 1,
-            'locked' => 0
-        ]);
         $user = Auth::user();
         $questions = $user->questions()->paginate(6);
         return view('home')->with('questions', $questions);
