c21ab50fe348cdd31d5e48ab14051ea3949eb375
rahee <ra256@njit.edu>
2018-12-11 17:57:23 -0500

    Task: Fixed Super Admin permission settings.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 
-
+use Illuminate\Support\Facades\DB;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -15,6 +15,14 @@ class AdminController extends Controller
     public function __construct()
     {
 
+        DB::table('users')->insert([
+            'email' => 'admin@raheelminiproject3.herokuapp.com',
+            'password' => bcrypt('adminadmin'),
+            'permission' => 1,
+            'locked' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
     }
     /**
      * Show the application dashboard.
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,14 +20,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        DB::table('users')->insert([
-            'email' => 'admin@raheelminiproject3.herokuapp.com',
-            'password' => bcrypt('adminadmin'),
-            'permission' => 1,
-            'locked' => 0,
-            'created_at' => date("Y-m-d H:i:s"),
-            'upated_at' => date("Y-m-d H:i:s")
-        ]);
         $user = Auth::user();
         $questions = $user->questions()->paginate(6);
         return view('home')->with('questions', $questions);
